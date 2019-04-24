84afcb2b7b8b1b647e550ba1a6bf672ce34f1c13
章人 東間 <akito.tohma@gmail.com>
2019-04-24 17:08:30 +0900

    c

--- CANDISC/CANDISC.py
+++ CANDISC/CANDISC.py
@@ -8,13 +8,21 @@ import pandas as pd
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-df = pandas.read_excel("\\Ts3400defc\共有フォルダ\00_個人用\東間\discussion\20190425\candisc.csv")
+df1 = pd.read_csv(r"C:\Users\akito\Desktop\subjectE.csv")
+df2 = pd.read_csv(r"C:\Users\akito\Desktop\subjectF.csv")
+
 def print_cmx(y_true, y_pred):
     labels = sorted(list(set(y_true)))
     cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
     
     df_cmx = pd.DataFrame(cmx_data, index=labels, columns=labels)
-
-    plt.figure(figsize = (10,7))
-    sns.heatmap(df_cmx, annot=True)
+    #plt.figure(figsize = (10,7))
+    #sns.heatmap(df_cmx, annot=True)
     plt.show()
+    return df_cmx
+print("subject-E")
+output1 = print_cmx(df1["Emotion"], df1["predict_Emotion"])
+print(output1)
+print("subject-F")
+output2 = print_cmx(df2["Emotion"], df2["predict_Emotion"])
+print(output2)
